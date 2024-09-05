4aba3818d7106ba573f857f62158132ae59b469c
Oli Sturm <oliver@oliversturm.com>
2024-09-05 15:06:57 +0100

    fix: remove old variable

--- CS/WinForms.Client/OrderItem.cs
+++ CS/WinForms.Client/OrderItem.cs
@@ -5,7 +5,6 @@
         public int Id { get; set; }
 
         public string ProductName { get; set; } = null!;
-        private decimal _unitPrice = 0;
         public decimal UnitPrice { get; set; }
         public int Quantity { get; set; }
         public float Discount { get; set; }
