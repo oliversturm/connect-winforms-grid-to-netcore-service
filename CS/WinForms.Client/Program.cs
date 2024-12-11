1cdb75cdac02bf706340c0fc99683a38891ef35d
Oli Sturm <oliver@oliversturm.com>
2024-12-11 15:51:45 +0000

    feat: startup handling with and without arguments

--- CS/WinForms.Client/Program.cs
+++ CS/WinForms.Client/Program.cs
@@ -3,16 +3,25 @@ using DevExpress.XtraEditors;
 using Microsoft.Win32;
 using System.IO;
 using System.IO.Pipes;
+using System.Runtime.InteropServices;
 
 namespace WinForms.Client
 {
+
     internal static class Program
     {
-        private const string pipeName = "WinAppDemoInstancePipe";
+        [DllImport("kernel32.dll")]
+        static extern bool AttachConsole(int dwProcessId);
+        private const int ATTACH_PARENT_PROCESS = -1;
+        private const string pipeName = "WinAppDemoProtocolMessagePipe";
 
         [STAThread]
         static int Main(string[] args)
         {
+            AttachConsole(ATTACH_PARENT_PROCESS);
+            Console.SetOut(new ConsoleTraceWriter());
+
+            Console.WriteLine("WinForms.Client starting up...");
 
             if (args.Length > 1)
             {
@@ -23,27 +32,24 @@ namespace WinForms.Client
             {
                 // Assuming that protocol messages should normally come in when another 
                 // instance of the app is already running, try first to send the message
-                // to the existing instance.
+                // to the existing app instance.
                 try
                 {
-                    using (var client = new NamedPipeClientStream(".", pipeName, PipeDirection.Out))
-                    {
-                        client.Connect(500);
-                        using (var writer = new StreamWriter(client) { AutoFlush = true })
-                        {
-                            writer.Write(args[0]);
-
-                            // Nothing else to do after sending, so quit.
-                            return 0;
-                        }
-                    }
+                    Console.WriteLine($"Sending protocol message: '{args[0]}'");
+                    SendProtocolMessage(args[0]);
+                    return 0;
                 }
                 catch (TimeoutException)
                 {
-                    // No existing instance found, so we need to start up and then handle the 
-                    // message ourselves.
+                    // No existing instance found. We will start up as normal,
+                    // but we won't handle the protocol message because this is an invalid state.
+                    // If a user logs in and a protocol message comes back, it cannot happen in 
+                    // this simple demo that there is no instance of the app.
+                    Console.Error.WriteLine("Invalid state: protocol message received without an existing instance.");
                 }
             }
+
+            Console.WriteLine("Starting UI with protocol listener");
 
             WindowsFormsSettings.SetPerMonitorDpiAware();
             WindowsFormsSettings.DefaultLookAndFeel.SetSkinStyle(SkinStyle.WXI);
@@ -51,6 +57,8 @@ namespace WinForms.Client
             Application.SetCompatibleTextRenderingDefault(false);
 
             RegisterProtocol();
+
+            StartProtocolMessageListener();
 
             Application.Run(new MainForm());
 
@@ -77,9 +85,32 @@ namespace WinForms.Client
             }
         }
 
+        static void SendProtocolMessage(string msg)
+        {
+            using var client = new NamedPipeClientStream(".", pipeName, PipeDirection.Out);
+            client.Connect(500);
+            using var writer = new StreamWriter(client) { AutoFlush = true };
+            writer.Write(msg);
+        }
+
+        static void StartProtocolMessageListener()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    using var server = new NamedPipeServerStream(pipeName, PipeDirection.In);
+                    server.WaitForConnection();
+                    using var reader = new StreamReader(server);
+                    var msg = reader.ReadToEnd();
+                    HandleProtocolMessage(msg);
+                }
+            });
+        }
+
         static void HandleProtocolMessage(string msg)
         {
-
+            Console.WriteLine($"Handling protocol message: '{msg}'");
         }
     }
 }
